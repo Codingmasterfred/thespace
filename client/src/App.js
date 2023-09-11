502678b6bb893f0b171ef86f11547e7971ff588c
Codingmasterfred <fredrickg777@gmail.com>
2023-09-10 21:51:15 -0700

    customizing frontend layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBarComponent from "./Navbar";
+import HomeComponent from "./Home";
+import UserProfileComponent from "./UserProfile";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBarComponent />
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="/UserProfile" element={<UserProfileComponent />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
