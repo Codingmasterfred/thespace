c575262b7d9000f694be395c9dd66dbc3bafd46b
Jayden Boyd <jaydenboydb1@gmail.com>
2023-09-13 03:47:37 -0500

    Added assets middleware and set up state mangement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import authReducer from './state';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import {persistStore, persistReducer, FLUSH, REHYDRATE,  PAUSE, PERSIST, PURGE, REGISTER} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
