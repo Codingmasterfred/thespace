2d7c1e7454e711951ac980e316f4037c69fca22f
Codingmasterfred <fredrickg777@gmail.com>
2023-09-14 09:25:04 -0700

    FreelancerProfilePage

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,22 @@ import NavBarComponent from "./Navbar";
 import HomeComponent from "./Home";
 import UserProfileComponent from "./UserProfile";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import CSS
+
 
 function App() {
   return (
     <Router>
       <NavBarComponent />
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<HomeComponent />} />
         <Route path="/UserProfile" element={<UserProfileComponent />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>
+    
   );
 }
 
