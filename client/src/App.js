5376b0e9b38bc5c38a5995496ccb76afc209fec0
Codingmasterfred <fredrickg777@gmail.com>
2023-09-14 13:05:08 -0700

    Starting On Corporate

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBarComponent from "./Navbar";
 import HomeComponent from "./Home";
-import UserProfileComponent from "./UserProfile";
+import UserProfileComponent from "./Freelancer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; // Import CSS
