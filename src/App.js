5eaf2ee1c0f8b9d12bb240bc9819acd49916ec2b
Jayden Boyd <jaydenboydb1@gmail.com>
2023-09-17 02:02:49 -0500

    Navbar done, can't test because fullName object is null rn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from './scenes/homePage';
+import HomePage from './scenes/homePage/homepage';
 import LoginPage from './scenes/loginPage';
 import ProfilePage from './scenes/profilePage';
-import './App.css';
+import { useMemo } from 'react';
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import {createTheme} from '@mui/material/styles';
+import { themeSettings } from './scenes/theme';
+
+
 
 function App() {
+  const mode = useSelector((state)=> state.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
       <BrowserRouter>
-      <Routes>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline/>
+       <Routes>
         <Route path = "/" element={<LoginPage />} />
         <Route path = "/home" element={<HomePage />} />
         <Route path = "/profile/:userid" element={<ProfilePage />} />
       </Routes>
+      </ThemeProvider>
     </BrowserRouter>
       
     </div>
