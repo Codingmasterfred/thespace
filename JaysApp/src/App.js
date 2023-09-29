451203ede6c1a39d39db7d0fd9082248ad37a863
Jayden Boyd <jaydenboydb1@gmail.com>
2023-09-29 11:47:16 -0500

    setting up user widget comp on homepage

--- JaysApp/src/App.js
+++ JaysApp/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from './scenes/homePage/homepage';
 import LoginPage from './scenes/loginPage/loginPage';
 import ProfilePage from './scenes/profilePage';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
 import {CssBaseline, ThemeProvider} from '@mui/material';
 import {createTheme} from '@mui/material/styles';
@@ -13,6 +13,7 @@ import { themeSettings } from './scenes/theme';
 function App() {
   const mode = useSelector((state)=> state.mode);
   const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
       <BrowserRouter>
@@ -20,7 +21,7 @@ function App() {
         <CssBaseline/>
        <Routes>
         <Route path = "/" element={<LoginPage />} />
-        <Route path = "/home" element={<HomePage />} />
+        <Route path = "/home" element={isAuth ? <HomePage /> : <Navigate to="/"/> } />
         <Route path = "/profile/:userid" element={<ProfilePage />} />
       </Routes>
       </ThemeProvider>
