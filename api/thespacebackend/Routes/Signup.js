03586b915d8eaad55c75ae4408257f59fcd60632
Eric Starks <starks.eric.d@gmail.com>
2023-10-03 15:19:47 -0500

    js

--- api/thespacebackend/Routes/Signup.js
+++ api/thespacebackend/Routes/Signup.js
@@ -5,14 +5,29 @@ require('dotenv').config();
 const connectToMongoDB = require('./middleware/db');
 //const seedDatabase = require("./seeders/seed");
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 // parse json requests 
 app.use(express.json());
-
+app.use(bodyParser.json());
 app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename:function(req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+const upload = multer({storage});
 
 // Middleware to connect to MongoDB
 //Using the next parameter lets us use different middleware,
@@ -33,6 +48,30 @@ app.use(async (req, res, next) => {
 app.get('/test', (req, res) => {
     res.send('This means that it works :)');
 });
+
+app.post('/auth/register', upload.single('picture'), (req, res) => {
+    try {
+        const {firstName, lastName, email, password, location, occupation} = req.body;
+
+        res.json({success: true, message: 'User registered successfully'});
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Registration failed'});
+    }
+});
+
+app.post('/auth/login', (req, res) => {
+    try {
+        const {email, password} = req.body;
+        res.json({
+            success:true,
+            user:{/*user data here*/},
+            token: 'your_access_token',
+        });
+    } catch (error) {
+        res.status(401).json({success: false, message: 'Login failed'});
+    }
+});
+
 //Listen for request
 app.listen(process.env.PORT, async () =>{
     console.log(` 🔅 Listening on Port`, process.env.PORT);
