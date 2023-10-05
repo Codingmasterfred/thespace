2ee23fec2009bf8df81590328c9b273a843ec04f
Daron Faulkner Jr <dj200099@gmail.com>
2023-10-05 15:12:37 -0500

    freelance routes completed

--- api/server.js
+++ api/server.js
@@ -7,7 +7,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const Jobs = require("./models/jobListingsModel");
+const Freelance = require("./models/freelanceTalentModel");
 const mongoose = require("mongoose");
+const FreelanceTalent = require("./models/freelanceTalentModel");
 const app = express();
 
 // parse json requests
@@ -91,6 +93,62 @@ app.delete("/joblistings", async (req, res) => {
 
   const deletedJob = await Jobs.findByIdAndDelete(jobID)
   res.send(deletedJob)
+})
+
+//===================
+app.post("/freelancetalent", async (req, res) => {
+
+  // Establishing a connection with MongoDb
+  // await connection();
+
+  let Talent = req.body;
+
+  await Freelance.insertMany(Talent).then(() => {
+    console.log("Freelance Talent has been added.");
+  });
+
+
+
+  res.send(Talent);
+});
+
+app.get("/freelancetalent", async (req, res) => {
+  const Talent = await Freelance.find()
+  res.send(Talent)
+});
+
+app.put("/freelancetalent", async (req, res) => {
+
+  try{
+
+    const talentID = req.body._id
+
+    const updatedTalent = await Freelance.findByIdAndUpdate(
+      talentID,
+      req.body,
+      {new: true}
+    );
+
+    if(!updatedTalent){
+      return res.status(404).json({ message: "Freelance Talent not found"});
+    }
+
+    res.json(updatedTalent);
+
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Internal Server error"})
+  }
+
+})
+
+app.delete("/freelancetalent", async (req, res) => {
+
+  const talentID = req.body._id
+
+  const deletedTalent = await Jobs.findByIdAndDelete(talentID)
+  res.send(deletedTalent)
 })
 //<------------The end of Daron making routes------------------>
 
