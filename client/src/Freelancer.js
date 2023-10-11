6bfeafcdade9357ff98a1d38de9b5a2e3ed599a9
Codingmasterfred <fredrickg777@gmail.com>
2023-10-11 07:29:51 -0700

    copy

--- client/src/Freelancer.js
+++ client/src/Freelancer.js
@@ -45,6 +45,9 @@ function UserProfileComponent() {
     const [showNextArrow, setShowNextArrow] = useState(false)
     const [showPreviousArrow, setShowPreviousArrow] = useState(false)
 
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
+
     const [currentIndex, setCurrentIndex] = useState(0);
 
     // Define the state variable without initialization
@@ -63,6 +66,8 @@ function UserProfileComponent() {
                 // Initialize with a different value for other cases
                 setEndIndex(3);
             }
+            setScreenWidth(window.innerWidth);
+            setScreenHeight(window.innerHeight);
     
             hasCodeRun.current = true; // Set the flag to true after running the code
         }
@@ -428,7 +433,7 @@ console.log(ListOfPortfolioFiles)
 
 
 return (
-    <div id="UserProfileComponentContainer">
+    <div id="UserProfileComponentContainer" >
         <div id="UserProfileContent" style={isSmallComputerScreen ? { width: "100%", flexDirection: "column-reverse", } : {}}>
             {/* <h1 id="profileH1">Profile</h1> */}
             <div id="SnapShotAndResumeContainer" style={{ width: isSmallComputerScreen && "100%" }}>
@@ -446,14 +451,26 @@ return (
                     )
                 }
                     {ListOfPortfolioFiles.slice(currentIndex, endIndex).map((Portfolio, index) => (
-                        <div id="PortfolioItems" key={index}>
+                        <div id="PortfolioItems"  style={{width: isSmallComputerScreen && isForPhone
+                        && "70%"}} key={index}>
                         
                         
                             {(!Portfolio.deleted && Portfolio.created && Portfolio.file) ? (
-                                <div style={{ position: "relative", width: "100%", height: "120px" }}>
+                                <div style={{ position: "relative",  width: isSmallComputerScreen && !isForPhone
+                                ? "90%" // When it's a small computer screen
+                                : isForPhone
+                                ? "100%" // When it's for a phone
+                                : isForPhone && !isSmallComputerScreen
+                                ? "181.229px" // When none of the conditions match
+                                : "auto",
+                                height:isSmallComputerScreen && !isForPhone
+                              ? "137px"
+                              :isForPhone && !isSmallComputerScreen
+                              ? "100%"
+                              : "100%", padding:" 1px 15px"  }}>
                                    { console.log("Here")}
                                     <iframe
-                                        style={{ overflow: "hidden", width: "100%", height: "100%", padding: "10px", overflowX: "hidden" }}
+                                        style={{ overflow: "hidden", width: "100%", height: "100%", paddingLeft:"10px", overflowX: "hidden" }}
                                         src={Portfolio.file}
                                         allowFullScreen
                                     />
@@ -512,144 +529,9 @@ return (
                 </div>
 
 
-
-
-
-                {/* <div id="PortfolioItems" > 
-                            {file ? (
-                                <div style={{ position: "relative", width: "100%", height: "100%" }}>
-                                    <iframe
-                                        style={{ overflow: "hidden", width: "100%", height: "100%" }}
-                                        src={file}
-                                        allowFullScreen
-                                    />
-                                    <button
-                                        onClick={() => {
-                                            setFile(null);
-                                        }}
-                                        style={{
-                                            position: "absolute",
-                                            top: 1, // Adjust top position as needed
-                                            left: 5, // Adjust right position as needed
-                                            zIndex: 1, // Ensure the button is on top of the iframe
-                                            backgroundColor: "white", // Set button background color
-                                            borderRadius: "50%", // Make it circular if needed
-                                            border: "1px solid #ccc", // Add a border
-                                            padding: "5px", // Adjust padding as needed
-                                        }}
-                                    >
-                                        <i className="fas fa-trash-alt"></i>
-                                    </button>
-                                </div>
-                            ) : (
-                                <label htmlFor="fileInput2" className="file-label">
-                                    
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="60%" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
-                                        <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2Zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672Z" />
-                                        <path d="M13.5 9a.5.5 0 0 1 .5.5V11h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V12h-1.5a.5.5 0 0 1 0-1H13V9.5a.5.5 0 0 1 .5-.5Z" />
-                                    </svg>
-                                    <input
-                                        type="file"
-                                        className="file-input"
-                                        onChange={handleFileChange}
-                                        id="fileInput2"
-
-                                        style={{ display: "none" }}
-                                    />
-                                </label>
-                            )}
-                        </div>
-                        <div>
-                            {file2 ? (
-                                <div style={{ position: "relative", width: "100%", height: "100%" }}>
-                                    <iframe
-                                        style={{ overflow: "hidden", width: "100%", height: "100%" }}
-                                        src={file2}
-                                        allowFullScreen
-                                    />
-                                    <button
-                                        onClick={() => {
-                                            setFile2(null);
-                                        }}
-                                        style={{
-                                            position: "absolute",
-                                            top: 1, // Adjust top position as needed
-                                            left: 5, // Adjust right position as needed
-                                            zIndex: 1, // Ensure the button is on top of the iframe
-                                            backgroundColor: "white", // Set button background color
-                                            borderRadius: "50%", // Make it circular if needed
-                                            border: "1px solid #ccc", // Add a border
-                                            padding: "5px", // Adjust padding as needed
-                                        }}
-                                    >
-                                        <i className="fas fa-trash-alt"></i>
-                                    </button>
-                                </div>
-                            ) : (
-                                <label htmlFor="fileInput2" className="file-label">
-                                   
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="60%" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
-                                        <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2Zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672Z" />
-                                        <path d="M13.5 9a.5.5 0 0 1 .5.5V11h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V12h-1.5a.5.5 0 0 1 0-1H13V9.5a.5.5 0 0 1 .5-.5Z" />
-                                    </svg>
-                                    <input
-                                        type="file"
-                                        className="file-input"
-                                        onChange={handleFileChange2}
-                                        id="fileInput2"
-
-                                        style={{ display: "none" }}
-                                    />
-                                </label>
-                            )}
-                        </div>
-                        <div>
-                            {file3 ? (
-                                <div style={{ position: "relative", width: "100%", height: "100%" }}>
-                                    <iframe
-                                        style={{ overflow: "hidden", width: "100%", height: "100%" }}
-                                        src={file3}
-                                        allowFullScreen
-                                    />
-                                    <button
-                                        onClick={() => {
-                                            setFile3(null);
-                                        }}
-                                        style={{
-                                            position: "absolute",
-                                            top: 1, // Adjust top position as needed
-                                            left: 5, // Adjust right position as needed
-                                            zIndex: 1, // Ensure the button is on top of the iframe
-                                            backgroundColor: "white", // Set button background color
-                                            borderRadius: "50%", // Make it circular if needed
-                                            border: "1px solid #ccc", // Add a border
-                                            padding: "5px", // Adjust padding as needed
-                                        }}
-                                    >
-                                        <i className="fas fa-trash-alt"></i>
-                                    </button>
-                                </div>
-                            ) : (
-                                <label htmlFor="fileInput3" className="file-label">
-                                    
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="60%" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
-                                        <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2Zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672Z" />
-                                        <path d="M13.5 9a.5.5 0 0 1 .5.5V11h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V12h-1.5a.5.5 0 0 1 0-1H13V9.5a.5.5 0 0 1 .5-.5Z" />
-                                    </svg>
-                                    <input
-                                        type="file"
-                                        className="file-input"
-                                        onChange={handleFileChange3}
-                                        id="fileInput3"
-
-                                        style={{ display: "none" }}
-                                    />
-                                </label>
-                            )}
-                            </div>*/}
                 <div style={
                     isSmallComputerScreen && isForPhone
-                        ? { margin: "10px", display: "flex", flexDirection: "column", justifyContent: "space-between", height: "150px", marginTop: "50px", border: "1px solid black", }
+                        ? { margin: "10px", display: "flex", flexDirection: "column", justifyContent: "space-between", height: "150px", marginTop: "50px",alignSelf:"center",width:"95%" }
                         : { margin: "10px", display: "flex", justifyContent: "space-evenly", }
                 }>
                     <input onChange={Link1function}
@@ -670,9 +552,9 @@ return (
                         }} placeholder="Link"></input>
 
                 </div>
-                <div id="ResumeContainer">
+                <div id="ResumeContainer" style={isForPhone && isSmallComputerScreen ? {marginTop:"50px"} :{}}>
                     <h4>Pitch</h4>
-                    <textarea onChange={(e) => { setPitch(e.target.value) }} id="ResumeTextArea"></textarea>
+                    <textarea onChange={(e) => { setPitch(e.target.value) }} id="ResumeTextArea" style={isForPhone && isSmallComputerScreen ? {height:"150px"} :{}}></textarea>
                     <Form.Group controlId="formFile" className="mb-3">
                         <Form.Label>< h4>Upload Resume</h4></Form.Label>
                         <Form.Control onChange={UploadResume} type="file" />
